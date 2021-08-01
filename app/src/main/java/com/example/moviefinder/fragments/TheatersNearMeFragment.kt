a0d15b8417100992b4e1a0f3e6390ab758c0cc47
Antonio Salazar III <salazar3antonio@gmail.com>
2021-08-01 10:33:28 -0500

    New Request Permission Flow
    Removed the deprecated flow and installed an updated version.
    Handled events if user denies GPS permission

--- app/src/main/java/com/example/moviefinder/fragments/TheatersNearMeFragment.kt
+++ app/src/main/java/com/example/moviefinder/fragments/TheatersNearMeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.moviefinder.fragments
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
@@ -8,6 +9,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -31,6 +35,16 @@ class TheatersNearMeFragment : Fragment() {
     private lateinit var mLocationProviderClient: FusedLocationProviderClient
     private lateinit var mTheaterListAdapter: TheaterListAdapter
     private var mUserOrigin = String()
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Timber.i("GPS Permission is Granted")
+                searchForTheatersWithUserLocation()
+            } else {
+                Timber.i("GPS Permission is Denied")
+                Toast.makeText(context, "You can also search by ZIP code.", Toast.LENGTH_LONG).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,17 +76,18 @@ class TheatersNearMeFragment : Fragment() {
             mUserOrigin = zipCode.text.toString()
 
             if (mUserOrigin.isEmpty()) {
-                Toast.makeText(context, getString(R.string.toast_text_validate_zip_code), Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    context,
+                    getString(R.string.toast_text_validate_zip_code),
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
                 mMovieViewModel.callTheaterSearchAPI(mUserOrigin)
             }
         }
 
         binding.btnMyLocation.setOnClickListener {
-            requestPermissions(
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                Constants.GPS_PERMISSION_REQUEST_CODE
-            )
+                onClickRequestPermission()
         }
 
         mMovieViewModel.mTheaterSearchResults.observe(viewLifecycleOwner, Observer {
@@ -81,47 +96,61 @@ class TheatersNearMeFragment : Fragment() {
 
         return binding.root
 
+    }
+
+    fun onClickRequestPermission() {
+
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                searchForTheatersWithUserLocation()
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+
+
+        }
 
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    @SuppressLint("MissingPermission")
+    fun searchForTheatersWithUserLocation() {
+        mLocationProviderClient.lastLocation.addOnSuccessListener { deviceLocation ->
 
-        when (requestCode) {
+            if (deviceLocation == null) {
+                Toast.makeText(
+                    context,
+                    "Make sure GPS is enabled on your device and try again.",
+                    Toast.LENGTH_LONG
+                ).show()
 
-            Constants.GPS_PERMISSION_REQUEST_CODE ->
+                requestUserLocationUpdate()
 
-                if ((grantResults.isNotEmpty()) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    mLocationProviderClient.lastLocation.addOnSuccessListener { deviceLocation ->
-
-                        if (deviceLocation == null) {
-
-                            Toast.makeText(
-                                context,
-                                "Make sure GPS is on and try again.",
-                                Toast.LENGTH_LONG
-                            ).show()
-
-                            requestUserLocationUpdate()
-
-                        } else {
-                            Timber.i("LOCATION: " + deviceLocation.latitude + " " + deviceLocation.longitude)
-                            mUserOrigin =
-                                deviceLocation.latitude.toString() + "," + deviceLocation.longitude.toString()
-                            mMovieViewModel.callTheaterSearchAPI(mUserOrigin)
-                        }
-                    }
-
-                } else
-                    Toast.makeText(context, "GPS Permission is required", Toast.LENGTH_LONG).show()
+            } else {
+                Timber.i("LOCATION: " + deviceLocation.latitude + ", " + deviceLocation.longitude)
+                mUserOrigin =
+                    deviceLocation.latitude.toString() + "," + deviceLocation.longitude.toString()
+                mMovieViewModel.callTheaterSearchAPI(mUserOrigin)
+            }
         }
     }
 
+    @SuppressLint("MissingPermission")
     fun requestUserLocationUpdate() {
 
         mLocationCallback = object : LocationCallback() {
@@ -147,7 +176,6 @@ class TheatersNearMeFragment : Fragment() {
             Looper.getMainLooper()
         )
     }
-
 
 }
 
